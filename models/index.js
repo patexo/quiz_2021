0f98e9d9dc19961a4e89f553b88e4dc714bbaa0a
patexo <patexowin@hotmail.es>
2021-05-04 18:37:29 +0200

    2.1.0b Quizzes: modelos migraciones y seed

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+// Load ORM
+const Sequelize = require('sequelize');
+
+
+// Environment variable to define the URL of the data base to use.
+// To use SQLite data base:
+//    DATABASE_URL = sqlite:quiz.sqlite
+//DATABASE_URL:        shell variable with DB access URL
+//sqlite:quiz.sqlite:  SQLite DB in quiz.sqlite file
+const url = process.env.DATABASE_URL || "sqlite:quiz.sqlite";
+
+const sequelize = new Sequelize(url);
+
+// Quiz Model
+sequelize.import(path.join(__dirname, 'quiz'));
+
+
+module.exports = sequelize;
